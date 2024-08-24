2318757c5c6151a7e2c3c7b960099b7d0e80646e
Xymist <james@jamieduerden.me>
2024-08-24 22:31:15 +0100

    Extract stream contents, respond with PONG

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::{Shutdown, TcpListener},
+};
 
 fn main() {
-    println!("Logs from your program will appear here!");
+    bind_and_listen();
+}
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+fn bind_and_listen() {
+    let listener = TcpListener::bind("127.0.0.1:6380").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                let mut buf = String::new();
+                stream
+                    .set_read_timeout(Some(std::time::Duration::from_millis(100)))
+                    .unwrap();
+
+                loop {
+                    match stream.read_to_string(&mut buf) {
+                        Ok(byte_count) if byte_count > 0 => byte_count,
+                        Ok(_) => {
+                            println!("Received 0 bytes");
+                            break;
+                        }
+                        Err(_) => {
+                            break;
+                        }
+                    };
+                }
+
+                println!("Stream: {:?}", buf);
+
+                stream.write_all(b"+PONG\r\n").unwrap();
+                stream.flush().unwrap();
+                stream.shutdown(Shutdown::Both).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
