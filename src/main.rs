2c7eafefab34bf486479023a3afff817e700dc78
Xymist <james@jamieduerden.me>
2024-08-26 13:50:34 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,6 @@ fn handle_connection(stream: &mut std::net::TcpStream) {
     const BUFFER_SIZE: usize = 10;
     let mut agg = String::new();
     let mut buf = [0; BUFFER_SIZE];
-    stream.set_nonblocking(true).unwrap();
     let mut reader = io::BufReader::new(stream.try_clone().unwrap());
 
     loop {
@@ -42,6 +41,12 @@ fn handle_connection(stream: &mut std::net::TcpStream) {
             // This is the last segment, either a partial buffer or
             // completely empty if the last full buffer was a perfect fit.
             Ok(n) if n < BUFFER_SIZE => {
+                // If the buffer is empty and we didn't read anything last time,
+                // we're just holding the connection open for more commands.
+                if agg.is_empty() && n == 0 {
+                    continue;
+                }
+
                 let s = str::from_utf8(&buf[..n]).unwrap();
                 agg.push_str(s);
                 println!("agg: {:?}", agg);
@@ -51,25 +56,12 @@ fn handle_connection(stream: &mut std::net::TcpStream) {
                     stream.write_all(response.to_string().as_bytes()).unwrap();
                 }
                 agg.clear();
-
-                break;
             }
             // This is a full buffer, we need to keep reading.
             Ok(n) => {
                 let s = str::from_utf8(&buf[..n]).unwrap();
                 agg.push_str(s);
                 buf.fill(0);
-            }
-            Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
-                println!("agg: {:?}", agg);
-                let commands = parse_input(&agg);
-                for command in commands {
-                    let response = command.into_command().into_response();
-                    stream.write_all(response.to_string().as_bytes()).unwrap();
-                }
-                agg.clear();
-
-                break;
             }
             Err(e) => {
                 println!("error: {}", e);
