a847347a9d4511f4af4e26b37006415006188a7b
Xymist <james@jamieduerden.me>
2024-08-27 21:09:25 +0100

    Fix various bugs in RDB loader, successfully load example RDB

--- src/rdb.rs
+++ src/rdb.rs
@@ -32,6 +32,8 @@ impl DBEntry {
 pub struct Rdb {
     version: String,
     metadata: HashMap<String, String>,
+    db_hash_table_size: usize,
+    expiry_hash_table_size: usize,
     selected_db: u32,
     data: HashMap<String, DBEntry>,
     original_checksum: u64,
@@ -66,7 +68,6 @@ pub fn load_db() -> Result<Rdb> {
         bail!("Invalid magic string: {}", magic);
     } else {
         let version = str::from_utf8(&buf[5..])?;
-        println!("Using RDB format version: {}", version);
         db_data.version = version.to_string();
     }
 
@@ -89,34 +90,32 @@ pub fn load_db() -> Result<Rdb> {
             // the latter followed by a b11111010 (250) byte.
             let mut buf = [0; 1];
             file.read_exact(&mut buf)?;
-            let key = extract_value(buf[0], &mut file, ExpectedKind::String)?;
+            let key = extract_value(buf[0], &mut file)?;
 
             // Read the next byte, which should be the length of the value
             file.read_exact(&mut buf)?;
-            let value = extract_value(buf[0], &mut file, ExpectedKind::String)?;
+            let value = extract_value(buf[0], &mut file)?;
 
-            println!("Metadata: {} = {}", key, value);
             db_data.metadata.insert(key, value);
         } else if buf[0] == 0xFE {
             // Do a database selector section
             let selected_db = {
                 let mut buf = [0; 1];
                 file.read_exact(&mut buf)?;
-                extract_value(buf[0], &mut file, ExpectedKind::Integer)?
+                extract_value(buf[0], &mut file)?
             };
-            println!("Selected database: {}", selected_db);
             db_data.selected_db = selected_db.parse().unwrap_or(0);
         } else if buf[0] == 0xFB {
             // Do a resize database section
             let mut buf = [0; 1];
 
             file.read_exact(&mut buf)?;
-            let db_size = extract_value(buf[0], &mut file, ExpectedKind::Integer)?;
-            println!("Database hash table size: {}", db_size);
+            let db_size = extract_value(buf[0], &mut file)?;
+            db_data.db_hash_table_size = db_size.parse().unwrap_or(0);
 
             file.read_exact(&mut buf)?;
-            let expires_size = extract_value(buf[0], &mut file, ExpectedKind::Integer)?;
-            println!("Database expiry hash table size: {}", expires_size);
+            let expires_size = extract_value(buf[0], &mut file)?;
+            db_data.expiry_hash_table_size = expires_size.parse().unwrap_or(0);
         } else if buf[0] == 0xFF {
             // End of file. Read an eight-byte checksum and we're done.
             let mut buf = [0; 8];
@@ -126,13 +125,6 @@ pub fn load_db() -> Result<Rdb> {
 
             break;
         } else {
-            // Do a key-value pair section
-
-            // Find out if there is an expiry
-            let mut buf = [0; 1];
-            file.read_exact(&mut buf)?;
-
-            // THE BUG IS HERE - IF THERE IS NO EXPIRY, WE ARE ADVANCING WITHOUT READING THE LENGTH OF THE KEY
             let expiry = match buf[0] {
                 0xFD => {
                     let mut buf = [0; 4];
@@ -158,15 +150,21 @@ pub fn load_db() -> Result<Rdb> {
             };
 
             let key = {
-                let mut buf = [0; 1];
-                file.read_exact(&mut buf)?;
-                extract_value(buf[0], &mut file, ExpectedKind::String)?
+                let key_start_byte = if expiry.is_some() {
+                    let mut nbuf = [0; 1];
+                    file.read_exact(&mut nbuf)?;
+                    nbuf[0]
+                } else {
+                    buf[0]
+                };
+
+                extract_value(key_start_byte, &mut file)?
             };
 
             let value = {
                 let mut buf = [0; 1];
                 file.read_exact(&mut buf)?;
-                let value = extract_value(buf[0], &mut file, ExpectedKind::String)?;
+                let value = extract_value(buf[0], &mut file)?;
                 // TODO: not everything is a string, this needs correcting
                 RESPValue::SimpleString(value)
             };
@@ -175,13 +173,9 @@ pub fn load_db() -> Result<Rdb> {
         }
     }
 
-    Ok(db_data)
-}
+    println!("Loaded RDB file: {:#?}", db_data);
 
-#[derive(Debug, PartialEq)]
-enum ExpectedKind {
-    String,
-    Integer,
+    Ok(db_data)
 }
 
 // Incredibly stupid fucking encoding for how long the next
@@ -199,25 +193,22 @@ enum ExpectedKind {
 //     The uncompressed length is read from the stream using Length Encoding
 //     The next clen bytes are read from the stream
 //     Finally, these bytes are decompressed using LZF algorithm
-fn extract_value(byte: u8, file: &mut File, expected: ExpectedKind) -> Result<String> {
+fn extract_value(byte: u8, file: &mut File) -> Result<String> {
     let nullified = byte & 0b11000000;
 
     match nullified {
         0b00000000 => {
             let remaining_bits = byte & 0b00111111;
             let length = remaining_bits as usize;
+            if length == 0 {
+                return Ok(String::from("0"));
+            }
             let mut val = vec![0; length];
             file.read_exact(&mut val)?;
-            if expected == ExpectedKind::Integer {
-                let val = match length {
-                    0 => 0.to_string(),
-                    1 => u8::from_le_bytes(val.try_into().unwrap()).to_string(),
-                    2 => u16::from_le_bytes(val.try_into().unwrap()).to_string(),
-                    4 => u32::from_le_bytes(val.try_into().unwrap()).to_string(),
-                    _ => unreachable!(),
-                };
-                return Ok(val);
-            }
+            // In this specific case, we need to convert 0 bytes to '0' bytes
+            // because otherwise they get interpreted as string terminators
+            // rather than integer values.
+            val = val.iter().map(|&x| if x == 0 { b'0' } else { x }).collect();
             Ok(String::from_utf8(val)?)
         }
         0b01000000 => {
@@ -259,7 +250,7 @@ fn extract_value(byte: u8, file: &mut File, expected: ExpectedKind) -> Result<St
         0b11000011 => {
             let mut buf = [0; 1];
             file.read_exact(&mut buf)?;
-            let clen = extract_value(buf[0], file, ExpectedKind::Integer)?.parse::<usize>()?;
+            let clen = extract_value(buf[0], file)?.parse::<usize>()?;
             //let ulen = extract_value(buf[0], file)?.parse::<usize>()?;
             let mut compressed = vec![0; clen];
             file.read_exact(&mut compressed)?;
